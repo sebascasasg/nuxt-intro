e07f534cec48add514d5fa223bc6bfc37a510955
sebascasasg <sebascasas.developer@gmail.com>
2021-06-05 10:25:36 -0500

    Created visual components

---
+++ components/LargeCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <NuxtLink :to="`/products/${cardInfo.id}`" class="card">
+    <img
+      :src="require(`@/assets/images/${cardInfo.image || 'fe1.jpg'}`)"
+      alt=""
+      class="image"
+    />
+    <h3 class="header">{{ cardInfo.title }}</h3>
+    <p class="snippet">{{ cardInfo.snippet }}</p>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    cardInfo: {
+      type: Object
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  width: 31.5%;
+  height: 25rem;
+  border: none;
+  overflow: hidden;
+  padding: 0;
+  cursor: pointer;
+}
+.image {
+  height: 65%;
+  border-radius: 0.5rem;
+}
+.header {
+  font-size: 1.15rem;
+  margin-top: 0.4rem;
+}
+.snippet {
+  color: grey;
+}
+</style>
---
+++ components/SmallCardDisplay.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <h4 class="header">{{ cardSection.title }}</h4>
+    <div class="card-container">
+      <SmallCard
+        v-for="card in cardSection.cards"
+        :key="card.id"
+        :cardInfo="card"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cardSection: {
+      type: Object
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  padding: 2rem 0;
+}
+.header {
+  font-weight: 700;
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
+}
+.card-container {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+</style>

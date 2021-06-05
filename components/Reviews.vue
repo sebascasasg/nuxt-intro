a2e82508cf34d11e8c0502f5353b43e2274584ea
sebascasasg <sebascasas.developer@gmail.com>
2021-06-05 13:13:32 -0500

    Created visual components for reviews in product pages

---
+++ components/Reviews.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>customer Reviews</h3>
+    <div v-if="!$fetchState.pending">
+      <ReviewCard
+        v-for="reviewer in reviewers.results"
+        :key="reviewer.login.uuid"
+        :review="reviewer"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      reviewers: []
+    };
+  },
+  async fetch() {
+    this.reviewers = await fetch(
+      "https://randomuser.me/api/?results=5"
+    ).then(res => res.json());
+  }
+};
+</script>
+
+<style scoped></style>
